e6d27dd667eca7c573653440cf8e4c21f51dfa4a
vadim <sudkurve@mail.ru>
2021-06-22 17:02:19 +0300

    chore(settings): configured some configs for eslint & prettier #2

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'jest', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+  ],
+  rules: {
+    "quotes": ["error", "single"],
+    "no-extra-semi": "error",
+    "semi": ["error", "always"],
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return <div></div>;
-}
+};
 
 export default App;
