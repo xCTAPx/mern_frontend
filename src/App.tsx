ee642e1dc2772fe2676d92d1285de61170cce76f
vadim <sudkurve@mail.ru>
2021-07-21 02:30:53 +0300

    feat(preprocessor): added scss instead of usual css #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {Router} from './Navigation';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Navigation';
+import './App.scss';
 
 const App: React.FC = () => (
   <BrowserRouter>
--- src/Routes/Login/Login.tsx
+++ src/Routes/Login/Login.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import styles from './Login.module.css';
+import styles from './Login.module.scss';
 
 export const Login: React.FC = () => {
-    const mode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' 
-    ?
-     'development' 
-     :
-     'production';
+  const mode =
+    !process.env.NODE_ENV ||
+    process.env.NODE_ENV === 'development'
+      ? 'development'
+      : 'production';
 
-    return <h6 className={styles.text}>Login Page ({mode})</h6>;
+  return (
+    <h6 className={styles.text}>Login Page ({mode})</h6>
+  );
 };
