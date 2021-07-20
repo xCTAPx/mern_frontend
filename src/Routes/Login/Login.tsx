ef69e570d88461d09538364d1ea8cd22044f207d
xCTAPx <sudkurve@mail.ru>
2021-07-21 02:22:32 +0300

    feat(styles|routes|env): added styles, routes & env mode #5

---
+++ src/Routes/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+export const Login: React.FC = () => {
+    const mode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' 
+    ?
+     'development' 
+     :
+     'production';
+
+    return <h6 className={styles.text}>Login Page ({mode})</h6>;
+};
