81199fe8de916c647f03a1035893a96350b4eada
xCTAPx <45179728+xCTAPx@users.noreply.github.com>
2021-08-05 23:13:08 +0300

    Login page (#5)
    * feat(login page): implemented login page base markup #6
    
    * fix(login page): added bootstrap mode tooltip #6
    
    * fix(login): fixed some styles in login page #6

--- src/Routes/Login/Login.tsx
+++ src/Routes/Login/Login.tsx
@@ -5,13 +5,24 @@ import { Input } from '../../components';
 export const Login: React.FC = () => {
   return (
     <section className={styles.background}>
-      <h6 className={styles.title}>Login Page</h6>
-      <div className={styles.form}>
-        <div className={styles.inputWrapper}>
-          <Input placeholder="E-mail" />
-        </div>
-        <div className={styles.inputWrapper}>
-          <Input placeholder="Password" type="password" />
+      <div className={styles.loginWrapper}>
+        <h6 className={styles.title}>Login Page</h6>
+        <div className={styles.form}>
+          <div className={styles.inputWrapper}>
+            <Input placeholder="E-mail" />
+          </div>
+          <div className={styles.inputWrapper}>
+            <Input placeholder="Nickname" />
+          </div>
+          <div className={styles.inputWrapper}>
+            <Input placeholder="Password" type="password" />
+          </div>
+          <div className={styles.inputWrapper}>
+            <Input
+              placeholder="Confirm password"
+              type="password"
+            />
+          </div>
         </div>
       </div>
     </section>
